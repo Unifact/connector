71bec204c7fde98a24d63bc57becbe80b2592ebd
Rob van Bentem <vanbentem@unifact.eu>
2015-10-05 11:35:25 +0200

    Implemented connector:run command

--- src/Console/RunCommand.php
+++ src/Console/RunCommand.php
@@ -3,9 +3,9 @@
 namespace Unifact\Connector\Console;
 
 use Illuminate\Console\Command;
+use Illuminate\Console\OutputStyle;
 use Unifact\Connector\Events\ConnectorRegisterHandlerEvent;
 use Unifact\Connector\Events\ConnectorRunCronEvent;
-use Unifact\Connector\Events\ConnectorRunJobEvent;
 use Unifact\Connector\Handler\Manager;
 
 class RunCommand extends Command
@@ -25,12 +25,27 @@ class RunCommand extends Command
         $manager = new Manager();
 
         // Allow handlers to register at the Manager
+        $this->out('Registering handlers.');
         \Event::fire(new ConnectorRegisterHandlerEvent($manager));
 
         // Run cronjobs
+        $this->out('Running cronjobs.');
         \Event::fire(new ConnectorRunCronEvent());
 
         // Handle jobs
+        $this->out('Handling Jobs.');
         //\Event::fire(new ConnectorRunJobEvent());
+    }
+
+    /**
+     * @param $text
+     * @param bool|true $newLine
+     * @param int $style
+     */
+    protected function out($text, $newLine = true, $style = OutputStyle::OUTPUT_NORMAL)
+    {
+        if ($this->option('verbose')) {
+            $this->output->write('--> '.$text, $newLine, $style);
+        }
     }
 }
