baf4caa726c1db0d6c3ac936ae3b025922422fb4
Rob van Bentem <vanbentem@unifact.eu>
2015-10-19 15:40:07 +0200

    added prefix to connector routes.

--- readme.md
+++ readme.md
@@ -3,3 +3,11 @@
 #### Intro
 
 Connector package for Laravel 5.1
+
+
+#### ENV settings
+
+- `CONNECTOR_DOMAIN` = The full domain the connector routes are available on (default: 'connector.localhost')
+- `CONNECTOR_PREFIX` = Prefix so we don't get route collisions with app routes (default: 'cnr')
+- `CONNECTOR_USER` = Username to access the connector
+- `CONNECTOR_PASS` = Password to access the connector
--- routes/routes.php
+++ routes/routes.php
@@ -2,15 +2,21 @@
 
 $domain = env('CONNECTOR_DOMAIN', sprintf('%s.%s', 'connector', env('DOMAIN', 'localhost')));
 $namespace = '\Unifact\Connector\Http\Controllers';
+$prefix = env('CONNECTOR_PREFIX', 'cnr');
 
 // No auth needed
-Route::group(['domain' => $domain, 'namespace' => $namespace, 'middleware' => ['connector.auth']], function () {
+Route::group([
+    'domain' => $domain,
+    'prefix' => $prefix,
+    'namespace' => $namespace,
+    'middleware' => ['connector.auth']
+], function () {
     Route::get('test', function () {
         return 'test';
     });
 });
 
 // Lock it up boyz
-Route::group(['domain' => $domain, 'namespace' => $namespace], function () {
+Route::group(['domain' => $domain, 'namespace' => $namespace, 'prefix' => $prefix], function () {
     Route::get('login', ['uses' => 'LoginController@login', 'as' => 'connector.login']);
 });
